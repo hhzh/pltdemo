432653d46d55ba7683eea520a742c29ac1e33fc0
hhzh <1138253815@qq.com>
2017-08-21 08:54:14 +0800

    init

---
+++ com/demo9.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 图像
+
+a = np.array(
+    [0.313660827978, 0.365348418405, 0.423733120134, 0.365348418405, 0.439599930621, 0.525083754405, 0.434733120134,
+     0.525083754405, 0.651536351379]).reshape(3, 3)
+
+plt.imshow(a,interpolation='nearest',cmap='bone',origin='lower')
+plt.colorbar(shrink=0.9)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
