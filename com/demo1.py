94392f9ee87dfac94cf2454a8e8dc04e0818dab0
hhzh <1138253815@qq.com>
2017-09-17 17:23:55 +0800

    add

--- com/demo1.py
+++ com/demo1.py
@@ -1,8 +1,15 @@
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# x = np.linspace(-1, 1, 50)
+# y = 2 * x + 1
+# # y=x**2
+# plt.plot(x, y)
+# plt.show()
 
-x = np.linspace(-1, 1, 50)
-y = 2 * x + 1
-# y=x**2
-plt.plot(x, y)
-plt.show()
+from nltk.book import *
+
+# print(text1)
+# print(text1.concordance('monstrous'))
+# print(len(text1))
+print(text1.generate())
